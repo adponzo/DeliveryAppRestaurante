bbc2b0f10b101a7b4f8cf3e34f38095403947709
Adalberto Ponzo <adponzo@gmil.com>
2020-10-06 19:47:57 -0300

    colocado https no environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   // api: 'http://localhost:3000'
-  api: 'http://api-appdelivery-com.umbler.net/'
+  api: 'https://api-appdelivery-com.umbler.net/'
 };
-
